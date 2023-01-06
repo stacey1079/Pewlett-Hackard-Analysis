0b5ecf8922be61b847078e663cf081c71dd434ac
stacey1079 <45715246+stacey1079@users.noreply.github.com>
2023-01-06 12:53:44 -0500

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -8,3 +8,30 @@ INTO nameyourtable
 FROM _______
 WHERE _______
 ORDER BY _____, _____ DESC;
+
+-- Deliverable 1
+-- retirees table
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirees
+FROM titles AS t
+JOIN employees AS e
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+--unique_titles table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, t.title
+INTO unique_titles
+FROM titles AS t
+JOIN employees AS e
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (t.to_date = '9999-01-01')
+ORDER BY e.emp_no, t.to_date DESC;
+
+-- retiring_titles table
+SELECT COUNT(title) AS "count", title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY "count" DESC;
