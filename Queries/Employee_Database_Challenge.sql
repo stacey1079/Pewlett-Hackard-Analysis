de91c91423db39c486a9e584a7c0bd234a8bb19b
stacey1079 <45715246+stacey1079@users.noreply.github.com>
2023-01-07 14:55:28 -0500

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,14 +1,3 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
-
 -- Deliverable 1
 -- retirees table
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
@@ -35,3 +24,16 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY "count" DESC;
+
+-- Deliverable 2
+-- mentorship table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title 
+INTO mentorship
+FROM dept_emp AS de
+JOIN employees AS e
+ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (t.to_date = '9999-01-01')
+ORDER BY e.emp_no;
